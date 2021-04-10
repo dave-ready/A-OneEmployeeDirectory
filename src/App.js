840e23dcfdc3131073a9c5c73acb5ed635f82328
David Ready <davis_ready@hotmail.com>
2021-04-09 22:39:14 -0400

    added component files + work on Card component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "./pages/Search/index";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
+
+
+
+import React, { useEffect, useState } from "react";
+import Card from './components/Card';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import Nav from './components/Nav'
