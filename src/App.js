7b2d1c5e931f2a6ead169c5dfcce3ad7d0a16dc3
David Ready <davis_ready@hotmail.com>
2021-04-10 09:36:01 -0400

    file restructure + missing boilerplate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Search from "./pages/Search/index";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-
-import React, { useEffect, useState } from "react";
-import Card from './components/Card';
-import Wrapper from './components/Wrapper';
-import Header from './components/Header';
-import Nav from './components/Nav'
+export default App;
